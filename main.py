d93b2376f3a7ae0d03a8411a701e544abd853acc
Daniel Pilloni <hello@stationzero.dk>
2025-09-03 15:02:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import socket
+from datetime import datetime
 
 PORT = 80
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("", PORT))
 s.listen(5)
 
+session = {
+    "client_header": "",
+    "client_ip": "",
+    "response_code": "",
+}
 
-def log_it(log):
+def log_it(client_ip, size, http_header):
     try:
         with open("log.txt", "a") as log_file:
-            log_file.write(log + "\n")
+            date = str(datetime.now())
+            log_file.write(f"{client_ip} - - {date} \" {http_header}\" {size}")
     except:
         pass
 
-def send_reply(response, body):
-    if response == "404":
-        header = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+def send_reply(status_code, body):
+    if status_code == "404":
+        header = "HTTP/1.1 404 Not Found\r\n\r\n"
 
-    elif response == "GET":
+    elif status_code == "GET":
         header = "HTTP/1.1 200 OK\r\n"
         header += "Content-Type: text/html\r\n\r\n"
 
+    elif status_code == "400":
+        header = "HTTP/1.1 400 Bad Request\r\n"
+        
     payload = header + body
-    connection.sendto(payload.encode(), client)
+    payload_encoded = payload.encode()
+    size = len(payload_encoded)
+    
+    connection.sendto(payload_encoded, client)
+
+    log_it(session["client_ip"], session["client_header"], session["response_code"], size)
 
     print("send response to client")
 
@@ -34,30 +49,31 @@ def unpack_header(header):
     file = request[1]
     httpv = request[2]
 
-    print("methode: " + methode)
-    print("file: " + file)
-    print("http v:" + httpv)
-
-    log_it(f"Request: {methode}, file: {file}, client IP: {client[0]}")
-
+    print(header_lines[0])
+    
     if methode == "GET":
         if file == "/":
-            file = "/index.html"
-        
+            file = "/index.html"        
         try:
             with open(file[1:]) as f:
                 response_txt = f.read()
         except:
             methode = "404"
-
-    log_it(f"Response: {methode}, file: {file}, client IP: {client[0]}")
+    else:
+        methode = "400"
+    
+    session["client_ip"] = client[0]
+    session["client_header"] = header_lines[0]
     
     send_reply(methode, response_txt)
 
 while True:
     connection , client = s.accept()
-    data = connection.recv(2048).decode()
-    if data != "":
-        unpack_header(data)
+    try:
+        data = connection.recv(2048).decode()
+        if data != "":
+            unpack_header(data)
+            connection.close()
+    except:
         connection.close()
-s.close()
+        s.close()
